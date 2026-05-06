fd1e9d758710ff1ce89fba95bc6fc50752e3f89b
xySaad <saadrm.dev@gmail.com>
2026-05-06 23:42:39 +0100

    fix(ui): add user avatar fallback

--- src/lib/components/image/Image.svelte
+++ src/lib/components/image/Image.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+	import type { DOMAttributes } from 'svelte/elements';
+
 	interface Props {
 		src: string;
 		alt?: string | null;
 		headers: Record<string, string>;
+		onerror?: DOMAttributes<HTMLImageElement>['onerror'];
 	}
 
-	const { src, alt, headers }: Props = $props();
+	const { src, alt, headers, onerror }: Props = $props();
 
 	const getImageUrl = async () => {
 		console.log(headers);
@@ -19,7 +22,7 @@
 {#await getImageUrl()}
 	<!--  -->
 {:then url}
-	<img src={url} {alt} />
+	<img src={url} {alt} {onerror} />
 {/await}
 
 <style>
--- src/lib/components/image/UserAvatar.svelte
+++ src/lib/components/image/UserAvatar.svelte
@@ -5,20 +5,31 @@
 	import { profileUserState } from '$lib/stores/user.svelte';
 	import { goto } from '$app/navigation';
 	import { resolve } from '$app/paths';
+	import Person from '$lib/assets/svg/person.svelte';
 
 	interface Props {
 		userLogin?: string | null;
 		avatarUrl?: string | null;
 	}
 	const { avatarUrl, userLogin }: Props = $props();
+
+	let error = $state(false);
 </script>
 
 <button onclick={() => goto(resolve(`/users/${userLogin}`))}>
-	<FallbackImage src={avatarUrl}>
-		<Image
-			src={`https://mapl.zone01oujda.ma/image/map/${userLogin}`}
-			alt={userLogin}
-			headers={{ 'X-TOKEN': `${get(profileUserState)?.token}` }}
-		/>
-	</FallbackImage>
+	{#if error}
+		<Person />
+	{:else}
+		<FallbackImage src={avatarUrl}>
+			<Image
+				src={`https://mapl.zone01oujda.ma/image/map/${userLogin}`}
+				alt={userLogin}
+				headers={{ 'X-TOKEN': `${get(profileUserState)?.token}` }}
+				onerror={() => (error = true)}
+			/>
+		</FallbackImage>
+	{/if}
 </button>
+
+<style>
+</style>
